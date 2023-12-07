417f7ce2d8bbcd3ee12129cf901e6522587c0683
nklt <146926614+ArinaLoseva@users.noreply.github.com>
2023-12-07 23:16:52 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(482, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(588, 57);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "шифровка П1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(750, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "шифровка У";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(869, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "расшифровка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(331, 183);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(482, 137);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(500, 183);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(482, 358);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(170, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ключ П";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "текст П";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ключ У1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(588, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "текст У";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(482, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "результат";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(226, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "шифровка П2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(482, 100);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(482, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "ключ У2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 490);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Tabel()
+        {
+            string alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя._,-";
+            string key = textBox1.Text;
+
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.RowCount = 6;
+
+            string inputWord = key.ToLower();
+            string resultString = inputWord;
+            foreach (char letter in alphabet)
+            {
+                if (!inputWord.Contains(letter))
+                {
+                    resultString += letter;
+                }
+            }
+            int k = 0;
+            for (int n = 0; n < 6; n++)
+            {
+                for (int m = 0; m < 6; m++)
+                {
+                    dataGridView1[m, n].Value = resultString[k];
+                    k++;
+                }
+            }
+            return resultString;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            string alphabet = Tabel();
+            string text = textBox2.Text;
+
+            if (text.Length % 2 != 0)
+            {
+                text = text + ".";
+                textBox2.Text = text;
+            }
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int a1 = 0, b1 = 0, a2 = 0, b2 = 0;
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 0; b < 6; b++)
+                    {
+                        if (text[i] == Convert.ToChar(dataGridView1[b, a].Value))
+                        {
+                            a1 = a;
+                            b1 = b;
+                        }
+
+                        if (text[i + 1] == Convert.ToChar(dataGridView1[b, a].Value))
+                        {
+                            a2 = a;
+                            b2 = b;
+                        }
+                    }
+                }
+
+                if (a1 != a2 && b1 != b2)
+                {
+                    textBox3.Text = textBox3.Text + dataGridView1[b2, a1].Value + dataGridView1[b1, a2].Value;
+                }
+                else
+                {
+                    if (a1 == a2)
+                    {
+                        b1 = b1 - 1;
+                        if (b1 < 0)
+                            b1 = 5;
+
+                        b2 = b2 - 1;
+                        if (b2 < 0)
+                            b2 = 5;
+                        textBox3.Text = textBox3.Text + dataGridView1[b1, a1].Value + dataGridView1[b2, a2].Value;
+                    }
+                    if (b1 == b2)
+                    {
+                        a1 = a1 - 1;
+                        if (a1 < 0)
+                            a1 = 5;
+
+                        a2 = a2 - 1;
+                        if (a2 < 0)
+                            a2 = 5;
+                        textBox3.Text = textBox3.Text + dataGridView1[b1, a1].Value + dataGridView1[b2, a2].Value;
+                    }
+                }
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //первая таблица
+            string alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя._,-";
+            string key1 = textBox3.Text;
+            dataGridView2.ColumnCount = 13;
+            dataGridView2.RowCount = 6;
+
+            string inputWord = key1.ToLower();
+            string resultString = inputWord;
+            foreach (char letter in alphabet)
+            {
+                if (!inputWord.Contains(letter))
+                {
+                    resultString += letter;
+                }
+            }
+            int k = 0;
+            for (int n = 0; n < 6; n++)
+            {
+                for (int m = 0; m < 6; m++)
+                {
+                    dataGridView2[m, n].Value = resultString[k];
+                    k++;
+                }
+            }
+
+            //вторая таблица
+            string key2 = textBox6.Text;
+            string inputWord2 = key2.ToLower();
+            string resultString2 = key2.ToLower();
+            foreach (char letter in alphabet)
+            {
+                if (!inputWord2.Contains(letter))
+                {
+                    resultString2 += letter;
+                }
+            }
+            int k2 = 0;
+            for (int n = 0; n < 6; n++)
+            {
+                for (int m = 7; m < 13; m++)
+                {
+                    dataGridView2[m, n].Value = resultString2[k2];
+                    k2++;
+                }
+            }
+
+            //шифровка
+            textBox5.Text = "";
+            string text = textBox4.Text;
+
+            if (text.Length % 2 != 0)
+            {
+                text = text + ".";
+                textBox4.Text = text;
+            }
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int a1 = 0, b1 = 0, a2 = 0, b2 = 0;
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 0; b < 6; b++)
+                    {
+                        if (text[i] == Convert.ToChar(dataGridView2[b, a].Value))
+                        {
+                            a1 = a;
+                            b1 = b;
+                        }
+                    }
+                }
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 7; b < 13; b++)
+                    {
+                        if (text[i + 1] == Convert.ToChar(dataGridView2[b, a].Value))
+                        {
+                            a2 = a;
+                            b2 = b;
+                        }
+                    }
+                }
+
+                if (a1 != a2 && b1 != b2)
+                {
+                    textBox5.Text = textBox5.Text + dataGridView2[b2, a1].Value + dataGridView2[b1, a2].Value;
+                }
+                else
+                {
+                    if (a1 == a2)
+                    {
+                        textBox5.Text = textBox5.Text + dataGridView2[b1 + 7, a1].Value + dataGridView2[b2 - 7, a1].Value;
+                    }
+                }
+                i++;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+                System.IO.File.WriteAllText(sfd.FileName, textBox5.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "";
+            string alphabet = Tabel();
+            string text = textBox2.Text;
+
+            if (text.Length % 2 != 0)
+            {
+                text = text + ".";
+                textBox2.Text = text;
+            }
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int a1 = 0, b1 = 0, a2 = 0, b2 = 0;
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 0; b < 6; b++)
+                    {
+                        if (text[i] == Convert.ToChar(dataGridView1[b, a].Value))
+                        {
+                            a1 = a;
+                            b1 = b;
+                        }
+
+                        if (text[i + 1] == Convert.ToChar(dataGridView1[b, a].Value))
+                        {
+                            a2 = a;
+                            b2 = b;
+                        }
+                    }
+                }
+
+                if (a1 != a2 && b1 != b2)
+                {
+                    textBox6.Text = textBox6.Text + dataGridView1[b2, a1].Value + dataGridView1[b1, a2].Value;
+                }
+                else
+                {
+                    if (a1 == a2)
+                    {
+                        b1 = b1 - 1;
+                        if (b1 < 0)
+                            b1 = 5;
+
+                        b2 = b2 - 1;
+                        if (b2 < 0)
+                            b2 = 5;
+                        textBox6.Text = textBox6.Text + dataGridView1[b1, a1].Value + dataGridView1[b2, a2].Value;
+                    }
+                    if (b1 == b2)
+                    {
+                        a1 = a1 - 1;
+                        if (a1 < 0)
+                            a1 = 5;
+
+                        a2 = a2 - 1;
+                        if (a2 < 0)
+                            a2 = 5;
+                        textBox6.Text = textBox6.Text + dataGridView1[b1, a1].Value + dataGridView1[b2, a2].Value;
+                    }
+                }
+                i++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = System.IO.File.ReadAllText(o.FileName, Encoding.Default);
+            }
+
+            textBox5.Text = "";
+            string text = textBox4.Text;
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int a1 = 0, b1 = 0, a2 = 0, b2 = 0;
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 7; b < 13; b++)
+                    {
+                        if (text[i] == Convert.ToChar(dataGridView2[b, a].Value))
+                        {
+                            a1 = a;
+                            b1 = b;
+                        }
+                    }
+                }
+
+                for (int a = 0; a < 6; a++)
+                {
+                    for (int b = 0; b < 6; b++)
+                    {
+                        if (text[i+1] == Convert.ToChar(dataGridView2[b, a].Value))
+                        {
+                            a2 = a;
+                            b2 = b;
+                        }
+                    }
+                }
+
+                if (a1 != a2 && b1 != b2)
+                {
+                    textBox5.Text = textBox5.Text + dataGridView2[b2, a1].Value + dataGridView2[b1, a2].Value;
+                }
+                else
+                {
+                    if (a1 == a2)
+                    {
+                        textBox5.Text = textBox5.Text + dataGridView2[b1-7, a1].Value + dataGridView2[b2+7, a1].Value;
+                    }
+                }
+                i++;
+            }
+        }
+    }
+}
